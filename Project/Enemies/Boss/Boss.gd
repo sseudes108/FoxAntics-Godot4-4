42a2632f2cba0ab88f21cd0453bdc6db4868624c
Senstur Games <senstur.art@gmail.com>
2023-10-06 07:56:26 -0300

    1.3

--- Project/Enemies/Boss/Boss.gd
+++ Project/Enemies/Boss/Boss.gd
@@ -1,10 +1,50 @@
 extends Node2D
 
+const HIT: String = "parameters/conditions/OnHit"
+const TRIGGER: String = "parameters/conditions/OnTrigger"
 
+@onready var stateMachine = $StateMachine
+@onready var body = $Body
+
+@export var health: int = 2
+@export var points: int = 5
+
+var invencible: bool = false
 
 func _ready():
 	pass
 
-
 func _process(delta):
 	pass
+
+func Trigger(area):
+	if stateMachine[TRIGGER] == false:
+		stateMachine[TRIGGER] = true
+
+func Hit(area):
+	TakeHit()
+
+func HitTween():
+	var tween = get_tree().create_tween()
+	tween.tween_property(body, "position", Vector2.ZERO, 1.0)
+
+func Invencible(key: bool):
+	print("Invencible: ", key)
+	invencible = key
+	stateMachine[HIT] = key
+
+func Health():
+	health -= 1
+	if health <= 0:
+		SignalManager.BossKilled.emit(points)
+		set_process(false)
+		queue_free()
+	print(health)
+
+func TakeHit():
+	if invencible == true:
+		return
+		
+	Invencible(true)
+	HitTween()
+	Health()
--- Project/Singletons/SignalManager.gd
+++ Project/Singletons/SignalManager.gd
@@ -2,3 +2,4 @@ extends Node
 
 signal EnemyHit(points: int, position: Vector2)
 signal pickedUp(points: int)
+signal BossKilled(points: int)
